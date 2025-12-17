4301216a7380f1e550eaaafa96b5de31a8591f67
Mohamed2006tvm <mohamed2006tvm@gmail.com>
2025-12-17 19:08:10 +0530

    Font style changed

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,15 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
-};
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         poppins: ['Poppins', 'sans-serif'],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
