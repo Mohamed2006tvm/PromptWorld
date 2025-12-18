33c30c29d31b0b34172f13882a7914ed585d496f
Mohamed2006tvm <mohamed2006tvm@gmail.com>
2025-12-18 12:21:13 +0530

    Login and logout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import LoginPage from './components/LoginPage'
+// import LoginPage from './components/LoginPage'
+import Routing from './components/Routing'
 
 const App = () => {
   return (
     <div>
-      <LoginPage/>
+        <Routing/>
     </div>
   )
 }
