9570c4dad1028de5c50d78b6d6e7b7bef71def4b
Mohamed2006tvm <mohamed2006tvm@gmail.com>
2025-12-20 11:34:16 +0530

    Side navbar completed , User profile updated dynamically, Theme button still unfinished

---
+++ Frontend/src/components/auth/Userprofile.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react"
+import { MoreVertical } from "lucide-react"
+import { supabase } from "../../Supabase"
+import SignOutButton from "./Signout"
+
+const UserProfile = () => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data, error } = await supabase.auth.getUser()
+      if (!error) {
+        setUser(data.user)
+        console.log(user.user_metadata.name)
+      }
+    }
+
+    getUser()
+  }, [])
+
+  if (!user) return null
+
+  const email = user.email
+  const name =
+    user.user_metadata?.full_name ||
+    email?.split("@")[0]?.toUpperCase()
+
+  const avatarLetter = name?.charAt(0).toUpperCase()
+
+  return (
+    <div className="  ">
+      <div
+        className="flex items-center justify-between gap-3 
+        rounded-[12px] p-3 cursor-pointer"      >
+        {/* Avatar */}
+        <div className="flex items-center gap-3">
+          <div
+            className="w-9 h-9 rounded-full bg-orange-500 
+            flex items-center justify-center font-bold text-white"
+          >
+            {avatarLetter}
+          </div>
+
+          {/* Name & Email */}
+          <div className="leading-tight">
+            <p className="text-sm font-semibold text-white">
+              {name}
+            </p>
+            <p className="text-xs text-[#868686] truncate max-w-[140px]">
+              {email}
+            </p>
+          </div>
+        </div>
+
+        {/* Menu
+        <MoreVertical size={18} className="text-[#868686]" /> */}
+
+          <SignOutButton/>
+      </div>
+    </div>
+  )
+}
+
+export default UserProfile
