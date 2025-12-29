200f5c8ef2f87080916277306c1c88b5f0f05def
Mohamed2006tvm <mohamed2006tvm@gmail.com>
2025-12-29 22:05:02 +0530

    Main dashboard

--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import SideNav from './SideNav'
 import Navbar from './Navbar'
+import DashboardHome from './DashboardHome'
+
 
 const Dashboard = () => {
   return (
     <>
+      <div className='flex'>
         <div className='flex items-start '>
-            <SideNav/>
-            <Navbar/>
+          <SideNav />
+          <Navbar />
         </div>
+        <div>
+          <DashboardHome/>
+        </div>
+      </div>
     </>
   )
 }
